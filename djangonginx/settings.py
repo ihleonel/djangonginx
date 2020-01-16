bd5c019d2b5873d656b5a4cd2e166e7574a06342
Ibarra Hector Leonel <ibarra.h.leonel@gmail.com>
2020-01-16 00:12:13 -0300

    Agregando servidor proxy

--- djangonginx/settings.py
+++ djangonginx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(@^tyi^!v75ur8s#l!mv28lq66fou!00o*x85u7)@16a9ov3!o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
